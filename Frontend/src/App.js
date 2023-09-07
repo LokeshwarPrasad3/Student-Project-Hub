7209ad74e00597b56f507aa6ca7dab545c65f85a
LokeshwarMERN <lokeshwar.prasad.cse@gmail.com>
2023-09-07 19:10:53 +0530

    fix - changed file name Home to Authentication

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import HomePage from './Pages/Home';
+import Authentication from './Pages/Authentication';
 
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage/>} />
+        <Route path="/auth" element={<Authentication />} />
       </Routes>
     </>
   );
