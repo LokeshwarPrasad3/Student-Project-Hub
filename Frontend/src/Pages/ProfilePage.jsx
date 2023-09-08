6310af9b2952f25fb55b0102a0a77e55f3c7e00c
LokeshwarMERN <lokeshwar.prasad.cse@gmail.com>
2023-09-09 00:10:43 +0530

    Enhance selection feature with synchronized input field in Profile Page

--- Frontend/src/Pages/ProfilePage.jsx
+++ Frontend/src/Pages/ProfilePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../CSS/Style.css'
 import Navbar from '../Components/Navbar';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
@@ -9,11 +9,47 @@ import CloseIcon from '@mui/icons-material/Close';
 import GradeIcon from '@mui/icons-material/Grade';
 import FactCheckIcon from '@mui/icons-material/FactCheck';
 import CallMissedOutgoingIcon from '@mui/icons-material/CallMissedOutgoing';
+import LinkIcon from '@mui/icons-material/Link';
 // used react-material components 
 // import Tabs from '@mui/material/Tabs';
 // import Tab from '@mui/material/Tab';
 
 const Profile = () => {
+
+  // when choosing technologies in options then store
+  const [selectedTechnology, setSelectedTechnology] = useState([]);
+
+  // handle when selected more options
+  const addTechnologies = (event) => {
+    const selectedValue = event.target.value;
+    // checkout previous values not store
+    if (!selectedTechnology.includes(selectedValue)) {
+      setSelectedTechnology([...selectedTechnology, selectedValue]);
+    }
+    console.log(selectedTechnology);
+  }
+
+
+  // when choosing Programming language in options then store
+  const [selectedProgramming, setSelectedProgramming] = useState([]);
+
+  // handle when selected more options
+  const addProgrammings = (event) => {
+    const selectedValue = event.target.value;
+    // checkout previous values not store
+    if (!selectedProgramming.includes(selectedValue)) {
+      setSelectedProgramming([...selectedProgramming, selectedValue]);
+    }
+    console.log(selectedTechnology);
+  }
+
+  // when clicked to close then remove
+  const handleRemoveOption = (name, state, setState) => {
+    const filteredOptions = state.filter((currName) => currName !== name);
+    setState(filteredOptions);
+  }
+
+
   return (
     <>
       <Navbar />
@@ -30,7 +66,7 @@ const Profile = () => {
 
 
         {/* user details strudents  */}
-        <div className="user_details text-white flex flex-wrap justify-center items-center px-12 gap-3 w-full h-full py-8 shadow-sm shadow-gray-600">
+        <div className="user_details text-white flex flex-wrap justify-center items-center px-0 gap-3 w-full h-full py-8 shadow-sm shadow-gray-600">
 
           {/* images name descr social links */}
           <div className="imp_show flex flex-col items-center gap-6 px-2 py-2 min-w-[30vw]">
@@ -52,7 +88,7 @@ const Profile = () => {
 
 
               {/* linlks of social media */}
-              <div className="social_links flex flex-col gap-2 w-full pl-[7vw] text-xl">
+              <div className="social_links flex flex-col gap-2 w-full pl-[5vw] text-xl">
                 {/* no of connections */}
                 <div className="user_connections flex gap-3 w-full  text-xl">
                   <Diversity1Icon />
@@ -60,26 +96,37 @@ const Profile = () => {
                   <span className='custom-values' >5</span>
                 </div>
 
+                {/* My Website link */}
+                <div className="website_link flex gap-3">
+                  <LinkIcon className='' />
+                  <a href="https://lokeshwar-creatives.netlify.app/" className='custom-values' target="_blank" rel="noopener noreferrer">
+                    {/* https://lokeshwar-creatives.netlify.app/ */}
+                    https://netlify.app/
+                  </a>
+                </div>
                 {/* linkedin link */}
                 <div className="linkedin_link flex gap-3">
                   <LinkedInIcon className='' />
-                  <a href="http://linkedin.com" className='custom-values' target="_blank" rel="noopener noreferrer">
-                    http://linkedin.com
+                  <a href="https://linkedin.com/in/lokeshwar-prasad-dewangan-7b2163211/" className='custom-values' target="_blank" rel="noopener noreferrer">
+                    {/* linkedin.com/in/lokeshwar-prasad-dewangan-7b2163211/ */}
+                    https://linkedin.com/
                   </a>
                 </div>
                 {/* linkedin link */}
                 <div className="github_link flex gap-3">
                   <GitHubIcon className='' />
-                  <a href="http://github.com" className='custom-values' target="_blank" rel="noopener noreferrer">
-                    http://github.com
+                  <a href="https://github.com/LokeshwarPrasad3" className='custom-values' target="_blank" rel="noopener noreferrer">
+                    {/* https://github.com/LokeshwarPrasad3 */}
+                    https://github.com/
                   </a>
                 </div>
 
                 {/* instagram link */}
                 <div className="instagram_link flex gap-3">
                   <InstagramIcon className='' />
-                  <a href="http://instagram.com" className='custom-values' target="_blank" rel="noopener noreferrer">
-                    http://instagram.com
+                  <a href="https://www.instagram.com/Lokeshwarprasad1/" className='custom-values' target="_blank" rel="noopener noreferrer">
+                    {/* https://www.instagram.com/Lokeshwarprasad1/ */}
+                    https://www.instagram.com/
                   </a>
                 </div>
               </div>
@@ -133,12 +180,16 @@ const Profile = () => {
           <div className="user_all_skills flex items-center flex-col px-3  pt-7 gap-9  text-xl min-w-[30vw]">
 
             {/* technology used */}
-            <div className="technologies_names flex  flex-col items-center gap-3">
+            <div className="technologies_names flex max-w-[82%] flex-col items-center gap-3">
               {/* technology listed options */}
               <div className="show_technologies flex gap-5 items-center justify-center">
                 <span className='small_heading' >Technology : </span>
                 {/* selections of technologies */}
-                <select className='cursor-pointer h-5 text-[1rem] text-black' name="" id="">
+                <select
+                  value={selectedTechnology}
+                  onChange={addTechnologies}
+                  multiple={false}
+                  className='cursor-pointer h-5 text-[1rem] text-black' name="" id="">
                   <option value="">_SELECT_Technology</option>
                   <option value="HTML">HTML</option>
                   <option value="CSS">CSS</option>
@@ -161,25 +212,28 @@ const Profile = () => {
               </div>
 
               {/* technology which is selected */}
-              <div className="selected_technology flex flex-wrap gap-1">
+              <div className="selected_technology max-w-[450px] flex flex-wrap gap-1 select-none">
 
-                {/* one selected */}
-                <div className="technology_box selected_options">
-                  <span>Javascript</span>
-                  <CloseIcon className='selected_options_close ' />
-                </div>
 
-                {/* one selected */}
-                <div className="technology_box selected_options">
-                  <span>React</span>
-                  <CloseIcon className='selected_options_close ' />
-                </div>
-
-                {/* one selected */}
-                <div className="technology_box selected_options">
-                  <span>Typescript</span>
-                  <CloseIcon className='selected_options_close ' />
-                </div>
+                {/* show all technologies which is selected by selectedTechnology state */}
+                {
+                  selectedTechnology.map((name) => {
+                    return (
+                      <>
+                        {/* one selected */}
+                        <div className="technology_box selected_options " key={name}>
+                          <span>{name}</span>
+                          <CloseIcon
+                            onClick={() => {
+                              const filteredOptions = selectedTechnology.filter((currName) => currName !== name);
+                              setSelectedTechnology(filteredOptions);
+                            }}
+                            className='selected_options_close ' style={{ height: '1.2rem', width: '1.2rem' }} />
+                        </div>
+                      </>
+                    )
+                  })
+                }
 
               </div>
 
@@ -189,10 +243,14 @@ const Profile = () => {
             {/* Core Programming Lnagugae */}
             <div className="programming_language_names flex flex-col gap-3">
               {/* programming language listed options */}
-              <div className="show_programming_language flex-2 flex-3 gap-3">
+              <div className="show_programming_language   flex-2 flex-3 gap-3">
                 <span className='small_heading' >Programming : </span>
                 {/* selections of Programming */}
-                <select className='cursor-pointer h-5 text-[1rem] text-black' name="" id="">
+                <select
+                  value={selectedProgramming}
+                  onChange={addProgrammings}
+                  multiple={false}
+                  className='cursor-pointer h-5 text-[1rem] text-black' name="" id="">
                   <option value="">_SELECT_Language</option>
                   <option value="C">C</option>
                   <option value="C++">C++</option>
@@ -203,25 +261,29 @@ const Profile = () => {
               </div>
 
               {/* technology which is selected */}
-              <div className="selected_programming_language flex gap-1">
+              <div className="selected_programming_language max-w-[450px] flex gap-1">
 
-                {/* one selected */}
-                <div className="language_box selected_options">
-                  <span>Java</span>
-                  <CloseIcon className='selected_options_close custom-transition' />
-                </div>
 
-                {/* one selected */}
-                <div className="language_box selected_options">
-                  <span>Python</span>
-                  <CloseIcon className='selected_options_close custom-transition' />
-                </div>
+                {/* show all prpogramming lang which is selected by selectedTechnology state */}
+                {
+                  selectedProgramming.map((name) => {
+                    return (
+                      <>
+                        {/* one selected */}
+                        <div className="language_box selected_options" key={name}>
+                          <span>{name}</span>
+                          <CloseIcon
+                            onClick={() => {
+                              const filteredOptions = selectedProgramming.filter((currName) => currName !== name);
+                              setSelectedProgramming(filteredOptions);
+                            }}
+                            className='selected_options_close custom-transition' style={{ height: '1.2rem', width: '1.2rem' }} />
+                        </div>
+                      </>
+                    )
+                  })
+                }
 
-                {/* one selected */}
-                <div className="language_box selected_options">
-                  <span>C++</span>
-                  <CloseIcon className='selected_options_close custom-transition' />
-                </div>
               </div>
             </div>
 
@@ -248,7 +310,7 @@ const Profile = () => {
 
 
         {/* all projects container with headings  */}
-        <div className="all_projects_container text-white px-16  flex flex-col items-center justify-center  w-full py-9 gap-5">
+        <div className="all_projects_container text-white px-16  flex flex-col items-center justify-center w-full py-9 gap-5 shadow-sm shadow-gray-600">
           {/* heding part */}
           <div className="disclaimers items-center flex w-full justify-center">
             <h2 className='font-signika font-semibold text-gray-300 text-2xl ' >CHECKOUT MY PROJECTS</h2>
@@ -263,7 +325,7 @@ const Profile = () => {
           <div className="projects_container flex py-4 gap-9 flex-wrap items-center justify-center ">
 
             {/* projecct box */}
-            <div className="project_box flex flex-col items-center bg-slate-600 rounded-md px-5 gap-2 min-w-[404px] py-6 shadow-md shadow-white">
+            <div className="project_box flex flex-col items-center bg-slate-600 rounded-md px-5 gap-2 min-w-[404px] py-6 shadow-md shadow-gray-200 ">
               <div className="status flex justify-center gap-2 items-center">
                 <span className="status text-xl text-green-500">OnGoing</span>
                 <CallMissedOutgoingIcon />
@@ -285,7 +347,7 @@ const Profile = () => {
 
 
             {/* projecct box */}
-            <div className="project_box flex flex-col items-center bg-slate-600 rounded-md px-5 gap-2 min-w-[404px] py-6 shadow-md shadow-white">
+            <div className="project_box flex flex-col items-center bg-slate-600 rounded-md px-5 gap-2 min-w-[404px] py-6 shadow-md shadow-gray-200">
               <div className="status flex justify-center gap-2 items-center">
                 <span className="status text-xl text-green-500">OnGoing</span>
                 <CallMissedOutgoingIcon />
@@ -307,7 +369,7 @@ const Profile = () => {
 
 
             {/* projecct box */}
-            <div className="project_box flex flex-col items-center bg-slate-600 rounded-md px-5 gap-2 min-w-[404px] py-6 shadow-md shadow-white">
+            <div className="project_box flex flex-col items-center bg-slate-600 rounded-md px-5 gap-2 min-w-[404px] py-6 shadow-md shadow-gray-200">
               <div className="status flex justify-center gap-2 items-center">
                 <span className="status text-xl text-green-500">OnGoing</span>
                 <CallMissedOutgoingIcon />
@@ -329,7 +391,7 @@ const Profile = () => {
 
 
             {/* projecct box */}
-            <div className="project_box flex flex-col items-center bg-slate-600 rounded-md px-5 gap-2 min-w-[404px] py-6 shadow-md shadow-white">
+            <div className="project_box flex flex-col items-center bg-slate-600 rounded-md px-5 gap-2 min-w-[404px] py-6 shadow-md shadow-gray-200">
               <div className="status flex justify-center gap-2 items-center">
                 <span className="status text-xl text-green-500">OnGoing</span>
                 <CallMissedOutgoingIcon />
